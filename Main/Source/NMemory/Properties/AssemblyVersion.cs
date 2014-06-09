a594d8490de0e1fd49c3fde77789f0476f93f17d
tamasflamich <none@none>
2014-06-09 22:42:37 +0200

    1.0.0 release

--- Main/Source/NMemory/Properties/AssemblyVersion.cs
+++ Main/Source/NMemory/Properties/AssemblyVersion.cs
@@ -36,4 +36,4 @@ using System.Reflection;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0-beta9")] 
+[assembly: AssemblyInformationalVersion("1.0.0")] 
