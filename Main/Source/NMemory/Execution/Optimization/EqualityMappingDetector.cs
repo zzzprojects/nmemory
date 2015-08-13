zzzprojects/nmemory

commit fce972a837aa4555c434c46988284ac478111e3c

Fix: logical join rewriters does not change join key datatypes anymore

Main/Source/NMemory/Execution/Optimization/EqualityMappingDetector.cs
@@ -30,11 +30,12 @@ namespace NMemory.Execution.Optimization
     using System.Linq.Expressions;
     using System.Reflection;
     using NMemory.Common;
+    using NMemory.Common.Expressions;
 
     public class EqualityMappingDetector : ExpressionVisitor
     {
-        private IList<MemberChain> leftMembers;
-        private IList<MemberChain> rightMembers;
+        private IList<IExpressionBuilder> leftMembers;
+        private IList<IExpressionBuilder> rightMembers;
 
         private Expression leftSource;
         private Expression rightSource;
@@ -43,11 +44,11 @@ namespace NMemory.Execution.Optimization
         {
             this.leftSource = left;
             this.rightSource = right;
-            this.leftMembers = new List<MemberChain>();
-            this.rightMembers = new List<MemberChain>();
+            this.leftMembers = new List<IExpressionBuilder>();
+            this.rightMembers = new List<IExpressionBuilder>();
         }
 
-        public MemberChain[] LeftMembers
+        public IExpressionBuilder[] LeftMembers
         {
             get
             {
@@ -55,7 +56,7 @@ namespace NMemory.Execution.Optimization
             }
         }
 
-        public MemberChain[] RightMembers
+        public IExpressionBuilder[] RightMembers
         {
             get
             {
@@ -69,12 +70,10 @@ namespace NMemory.Execution.Optimization
             {
                 BinaryExpression equal = expression as BinaryExpression;
 
-                // Detect the operands of the AND operation
-                // Check if the sides referred to members of different sources
+                // Detect the operands of the EQUAL operation
                 return 
-                    this.DetectAndOperand(equal.Left) && 
-                    this.DetectAndOperand(equal.Right) &&
-                    this.leftMembers.Count == this.rightMembers.Count;
+                    this.DetectEqualOperand(equal.Left) && 
+                    this.DetectEqualOperand(equal.Right);
             }
 
             if (expression.NodeType != ExpressionType.AndAlso)
@@ -82,50 +81,33 @@ namespace NMemory.Execution.Optimization
                 return false;
             }
 
+            // 'expression' is an AndAlso expression
             BinaryExpression binary = expression as BinaryExpression;
 
             return this.Detect(binary.Left) && this.Detect(binary.Right);
         }
 
-        private bool DetectAndOperand(Expression expression)
+        private bool DetectEqualOperand(Expression expression)
         {
-            expression = ExpressionHelper.SkipConversionNodes(expression);
+            return 
+                DetectEqualOperand(expression, this.leftSource, this.leftMembers) ||
+                DetectEqualOperand(expression, this.rightSource, this.rightMembers);
+        }
 
-            MemberExpression member = expression as MemberExpression;
+        private bool DetectEqualOperand(
+            Expression expression, 
+            Expression source,
+            IList<IExpressionBuilder> collector)
+        {
+            UnaryExpressionCloner expander = null;
 
-            if (member == null)
+            if (!UnaryExpressionCloner.TryCreate(expression, source, out expander))
             {
                 return false;
             }
 
-            return
-                this.DetectMember(member, this.leftSource, this.leftMembers) ||
-                this.DetectMember(member, this.rightSource, this.rightMembers);
-
-        }
-
-        private bool DetectMember(
-            MemberExpression member, 
-            Expression source,
-            IList<MemberChain> memberChainCollector)
-        {
-            List<MemberInfo> memberChain = new List<MemberInfo>();
-
-            do
-            {
-                memberChain.Add(member.Member);
-
-                if (member.Expression == source)
-                {
-                    memberChainCollector.Add(new MemberChain(memberChain));
-                    return true;
-                }
-
-                member = member.Expression as MemberExpression;
-            }
-            while (member != null);
-
-            return false;
+            collector.Add(expander);
+            return true;
         }
     }
 }

Main/Source/NMemory/Common/Expressions/IExpressionBuilder.cs
@@ -1,0 +1,33 @@
+﻿// ----------------------------------------------------------------------------------
+// <copyright file="IExpressionBuilder.cs" company="NMemory Team">
+//     Copyright (C) 2012-2013 NMemory Team
+//
+//     Permission is hereby granted, free of charge, to any person obtaining a copy
+//     of this software and associated documentation files (the "Software"), to deal
+//     in the Software without restriction, including without limitation the rights
+//     to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//     copies of the Software, and to permit persons to whom the Software is
+//     furnished to do so, subject to the following conditions:
+//
+//     The above copyright notice and this permission notice shall be included in
+//     all copies or substantial portions of the Software.
+//
+//     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//     IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//     FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//     AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//     LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//     THE SOFTWARE.
+// </copyright>
+// ---------------------------------------------------------------------------------
+
+namespace NMemory.Common.Expressions
+{
+    using System.Linq.Expressions;
+
+    public interface IExpressionBuilder
+    {
+        Expression Build(Expression source);
+    }
+}

Main/Source/NMemory/Common/Expressions/UnaryExpressionCloner.cs
@@ -1,0 +1,121 @@
+﻿// ----------------------------------------------------------------------------------
+// <copyright file="UnaryExpressionCloner.cs" company="NMemory Team">
+//     Copyright (C) 2012-2013 NMemory Team
+//
+//     Permission is hereby granted, free of charge, to any person obtaining a copy
+//     of this software and associated documentation files (the "Software"), to deal
+//     in the Software without restriction, including without limitation the rights
+//     to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//     copies of the Software, and to permit persons to whom the Software is
+//     furnished to do so, subject to the following conditions:
+//
+//     The above copyright notice and this permission notice shall be included in
+//     all copies or substantial portions of the Software.
+//
+//     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//     IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//     FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//     AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//     LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//     THE SOFTWARE.
+// </copyright>
+// ---------------------------------------------------------------------------------
+
+namespace NMemory.Common.Expressions
+{
+    using System.Linq.Expressions;
+    using NMemory.Common.Visitors;
+
+    internal class UnaryExpressionCloner : IExpressionBuilder
+    {
+        private readonly Expression root;
+        private readonly Expression unary;
+
+        private UnaryExpressionCloner(Expression original, Expression root)
+        {
+            this.unary = original;
+            this.root = root; 
+        }
+
+        public static bool TryCreate(
+            Expression unary, 
+            Expression root, 
+            out UnaryExpressionCloner result)
+        {
+            if (unary == null)
+            {
+                result = null;
+                return false;
+            }
+
+            Expression expr = unary;
+
+            do
+            {
+                // Check if we reached the root expression
+                if (expr == root)
+                {
+                    result = new UnaryExpressionCloner(unary, root);
+                    return true;
+                }
+            }
+            while (TryGetNextStep(expr, out expr)) ;
+
+            // Last chance: check for a default root node
+            if (root == null && IsValidDefaultNode(expr))
+            {
+                result = new UnaryExpressionCloner(unary, expr);
+                return true;
+            }
+
+            result = null;
+            return false;
+        }
+
+        public Expression Build(Expression source)
+        {
+            return ReplaceVisitor.Replace(
+                this.unary, 
+                x => x == this.root, 
+                x => source);
+        }
+
+        private static bool TryGetNextStep(Expression current, out Expression next)
+        {
+            UnaryExpression unary = current as UnaryExpression;
+
+            if (unary != null)
+            {
+                next = unary.Operand;
+                return true;
+            }
+
+            MemberExpression member = current as MemberExpression;
+
+            if (member != null)
+            {
+                next = member.Expression;
+
+                // The value of Expression if it represents a static member
+                return next != null;
+            }
+
+            // Return the input
+            next = current;
+            return false;
+        }
+
+        private static bool IsValidDefaultNode(Expression node)
+        {
+            switch (node.NodeType)
+            {
+                case ExpressionType.Constant:
+                //case ExpressionType.Parameter:
+                    return true;
+                default:
+                    return false;
+            }
+        }
+    }
+}

Main/Source/NMemory/Common/LinqJoinKeyHelper.cs
@@ -29,14 +29,15 @@ namespace NMemory.Common
     using System.Linq;
     using System.Text;
     using System.Linq.Expressions;
+    using NMemory.Common.Expressions;
 
     internal class LinqJoinKeyHelper
     {
         public static void CreateKeySelectors(
             Type outerType,
             Type innerType,
-            MemberChain[] outerKeyMembers,
-            MemberChain[] innerKeyMembers,
+            IExpressionBuilder[] outerKeyCreator,
+            IExpressionBuilder[] innerKeyCreator,
             out LambdaExpression outerKey,
             out LambdaExpression innerKey)
         {
@@ -50,12 +51,10 @@ namespace NMemory.Common
             List<Expression> outerKeyMemberAccess = new List<Expression>();
             List<Expression> innerKeyMemberAccess = new List<Expression>();
 
-            for (int i = 0; i < outerKeyMembers.Length; i++)
+            for (int i = 0; i < outerKeyCreator.Length; i++)
             {
-                Expression outerAccess = 
-                    outerKeyMembers[i].CreateExpression(outerKeyParam);
-                Expression innerAccess = 
-                    innerKeyMembers[i].CreateExpression(innerKeyParam);
+                Expression outerAccess = outerKeyCreator[i].Build(outerKeyParam);
+                Expression innerAccess = innerKeyCreator[i].Build(innerKeyParam);
 
                 // KEY PART: try to unify the type of the expressions
                 ExpressionHelper.TryUnifyValueTypes(ref outerAccess, ref innerAccess);

Main/Source/NMemory/NMemory.csproj
@@ -42,6 +42,8 @@
     <Compile Include="Common\CollectionExtensions.cs" />
     <Compile Include="Common\DefaultEntityMemberInfo`2.cs" />
     <Compile Include="Common\DefaultEntityMemberInfoServices`2.cs" />
+    <Compile Include="Common\Expressions\IExpressionBuilder.cs" />
+    <Compile Include="Common\Expressions\UnaryExpressionCloner.cs" />
     <Compile Include="Common\IEntityMemberInfoServicesProvider`2.cs" />
     <Compile Include="Common\IEntityMemberInfoServices`2.cs" />
     <Compile Include="Common\IEntityMemberInfo`2.cs" />

commit a9fb09bc5adc40c88cb2320cb857b8c5625ea7fc

Fixed a Bug in EqualityMappingDetector that was introduced with commit fce972a837aa4555c434c46988284ac478111e3c as one line was removed.

Main/Source/NMemory/Execution/Optimization/EqualityMappingDetector.cs
@@ -73,7 +73,8 @@ namespace NMemory.Execution.Optimization
                 // Detect the operands of the EQUAL operation
                 return 
                     this.DetectEqualOperand(equal.Left) && 
-                    this.DetectEqualOperand(equal.Right);
+                    this.DetectEqualOperand(equal.Right) &&
+                    this.leftMembers.Count == this.rightMembers.Count;
             }
 
             if (expression.NodeType != ExpressionType.AndAlso)