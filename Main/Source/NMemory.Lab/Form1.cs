055e32bdbc06efba73837f34aefba408ef6ab052
Jonathan Magnan <info@zzzprojects.com>
2018-08-22 16:58:18 -0400

    Fix release setting
    Fix release setting

--- Main/Source/NMemory.Lab/Form1.cs
+++ Main/Source/NMemory.Lab/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using NMemory.Tables;
 
 namespace NMemory.Lab
 {
@@ -15,6 +16,49 @@ namespace NMemory.Lab
         public Form1()
         {
             InitializeComponent();
+
+            MyDatabase myDatabase = new MyDatabase();
+
+            myDatabase.Members.Insert(new Member() {Id = "1", Name = "abc"});
+            var list = myDatabase.Members.Where(x => x.Name == "Abc").ToList();
+
+        }
+
+        public class MyDatabase : Database
+        {
+            public MyDatabase()
+            {
+                var members = this.Tables.Create<Member, string>(x => x.Id, null);
+                var groups = base.Tables.Create<Group, int>(g => g.Id, null);
+
+                this.Members = members;
+                this.Groups = groups;
+            }
+
+            public ITable<Member> Members { get; private set; }
+
+            public ITable<Group> Groups { get; private set; }
+        }
+
+        public class Member
+        {
+            public string Id { get; set; }
+
+            public string Name { get; set; }
+
+            public int? GroupId { get; set; }
+
+            public int GroupId2 { get; set; }
+        }
+
+        public class Group
+        {
+            public int Id { get; set; }
+
+            public int Id2 { get; set; }
+
+            public string Name { get; set; }
+
         }
     }
 }
