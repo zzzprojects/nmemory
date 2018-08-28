030cf4f7678f042482fb346c3ea8d32bd9c5c3c2
Jonathan Magnan <info@zzzprojects.com>
2018-08-28 14:39:44 -0400

    Update assembly version

--- Main/Source/NMemory.Net45/Properties/AssemblyVersion.cs
+++ Main/Source/NMemory.Net45/Properties/AssemblyVersion.cs
@@ -35,5 +35,5 @@ using System.Reflection;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.2")]
-[assembly: AssemblyInformationalVersion("2.0.2")]
+[assembly: AssemblyFileVersion("2.0.3")]
+[assembly: AssemblyInformationalVersion("2.0.3")]
