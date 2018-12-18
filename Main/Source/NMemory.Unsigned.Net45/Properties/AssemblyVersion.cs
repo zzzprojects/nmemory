decf664b7e0c5ef9f74a3b3be83b5669039c9ea4
Jonathan Magnan <info@zzzprojects.com>
2018-12-18 14:57:18 -0500

    Update test to allow to always run the unsigned one

--- Main/Source/NMemory.Unsigned.Net45/Properties/AssemblyVersion.cs
+++ Main/Source/NMemory.Unsigned.Net45/Properties/AssemblyVersion.cs
@@ -1,5 +1,5 @@
 ﻿// ----------------------------------------------------------------------------------
-// <copyright file="AssemblyVisibility.cs" company="NMemory Team">
+// <copyright file="AssemblyVersion.cs" company="NMemory Team">
 //     Copyright (C) NMemory Team
 //
 //     Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -22,4 +22,20 @@
 // </copyright>
 // ----------------------------------------------------------------------------------
 
-using System.Runtime.CompilerServices;
+using System.Reflection;
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyFileVersion("3.0.0")]
+[assembly: AssemblyInformationalVersion("3.0.0")]
+
+
