578216ba7db4c5a2dd2d934366b31380edfd3dc3
Jonathan Magnan <info@zzzprojects.com>
2022-02-03 11:36:30 -0500

    Fixed FileVersion missing for .NET Standard 20 project

--- Main/Source/NMemory.Net40/Properties/AssemblyVersion.cs
+++ Main/Source/NMemory.Net40/Properties/AssemblyVersion.cs
@@ -35,5 +35,5 @@ using System.Reflection;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("3.0.0.0")]
-[assembly: AssemblyFileVersion("3.1.3")]
-[assembly: AssemblyInformationalVersion("3.1.3")]
+[assembly: AssemblyFileVersion("3.1.4")]
+[assembly: AssemblyInformationalVersion("3.1.4")]
